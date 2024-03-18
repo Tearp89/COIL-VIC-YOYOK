bbfbc01d7f250bd580d3d5ed969e5d8a0ffdf9b1
Isabella Méndez <isabella.hmdez@gmail.com>
2024-03-18 00:34:13 -0600

    Interfaces faltantes, cambios en DAOS de admin y facilitador y cambios en test de admin y facilitador

--- Código/COIL_VIC/src/COIL_VIC_LOGIC/Interfaces/IUniversity.java
+++ Código/COIL_VIC/src/COIL_VIC_LOGIC/Interfaces/IUniversity.java
@@ -4,4 +4,6 @@ import COIL_VIC_LOGIC.Classes.University;
 
 public interface IUniversity {
     int addUniversity(University university);
+    int deleteUniversity(University university);
+    int updateUniversity(University university);
 }
