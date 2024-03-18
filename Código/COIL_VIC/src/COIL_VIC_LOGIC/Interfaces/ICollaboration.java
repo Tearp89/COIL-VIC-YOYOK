8c4cf62ee220607a1ce404e569af52fdf5270677
Isabella Méndez <isabella.hmdez@gmail.com>
2024-03-18 00:56:15 -0600

    Interfaces faltantes

---
+++ Código/COIL_VIC/src/COIL_VIC_LOGIC/Interfaces/ICollaboration.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package COIL_VIC_LOGIC.Interfaces;
+
+import COIL_VIC_LOGIC.Classes.Collaboration;
+
+/**
+ *
+ * @author isabe
+ */
+public interface ICollaboration {
+    int addCollaboration(Collaboration collaboration);
+    int deleteCollaboration(Collaboration collaboration);
+    int updateCollaboration(Collaboration collaboration);
+    
+}
