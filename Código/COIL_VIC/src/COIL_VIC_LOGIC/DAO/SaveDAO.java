84a907436f8891223a9426ad2f7d0c4b93ed5dfe
Drawiel <daur0704@outlook.com>
2024-03-22 03:14:46 -0600

    DAO para guardar JTables
    Creación de SaveDAO para guardar el contenido de una JTable en un archivo excel personalizado

---
+++ Código/COIL_VIC/src/COIL_VIC_LOGIC/DAO/SaveDAO.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package COIL_VIC_LOGIC.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import COIL_VIC_DataAccess.DatabaseManager;
+import java.awt.Desktop;
+import java.io.File;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+import org.apache.poi.ss.usermodel.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+/**
+ *
+ * @author daur0
+ */
+public class SaveDAO {
+    public void exportExcel(JTable t) throws IOException {
+        JFileChooser jFileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos de excel", "xls");
+        jFileChooser.setFileFilter(filter);
+        jFileChooser.setDialogTitle("Guardar archivo");
+        jFileChooser.setAcceptAllFileFilterUsed(false);
+        if (jFileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            String ruta = jFileChooser.getSelectedFile().toString().concat(".xls");
+            try {
+                File excelFile = new File(ruta);
+                if (excelFile.exists()) {
+                    excelFile.delete();
+                }
+                excelFile.createNewFile();
+                Workbook workbook = new HSSFWorkbook();
+                try (FileOutputStream file = new FileOutputStream(excelFile)) {
+                    Sheet sheet = workbook.createSheet("Mi hoja de trabajo 1");
+                    sheet.setDisplayGridlines(false);
+                    for (int f = 0; f < t.getRowCount(); f++) {
+                        Row row = sheet.createRow(f);
+                        for (int c = 0; c < t.getColumnCount(); c++) {
+                            Cell cell = row.createCell(c);
+                            if (f == 0) {
+                                cell.setCellValue(t.getColumnName(c));
+                            }
+                        }
+                    }
+                    int startRow = 1;
+                    for (int f = 0; f < t.getRowCount(); f++) {
+                        Row row = sheet.createRow(startRow);
+                        startRow++;
+                        for (int c = 0; c < t.getColumnCount(); c++) {
+                            Cell cell = row.createCell(c);
+                            if (t.getValueAt(f, c) instanceof Double) {
+                                cell.setCellValue(Double.parseDouble(t.getValueAt(f, c).toString()));
+                            } else if (t.getValueAt(f, c) instanceof Float) {
+                                cell.setCellValue(Float.parseFloat((String) t.getValueAt(f, c)));
+                            } else {
+                                cell.setCellValue(String.valueOf(t.getValueAt(f, c)));
+                            }
+                        }
+                    }
+                    workbook.write(file);
+                }
+                Desktop.getDesktop().open(excelFile);
+            } catch (IOException | NumberFormatException e) {
+                throw e;
+            }
+        }
+    }
+}
