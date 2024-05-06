3b762b75c1ad5dd5944013260393993876ab2c3c
Marla <121531470+Tearp89@users.noreply.github.com>
2024-05-06 13:38:29 -0600

    Alerta personalizada

---
+++ Código/COIL_VIC/src/logic/classes/ProfessorAcceptedAlert.java
@@ -0,0 +1,19 @@
+package logic.classes;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ProfessorAcceptedAlert extends Alert{
+    public ProfessorAcceptedAlert (AlertType alertType){
+        super(alertType);
+        Stage stage = (Stage) this.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("\"C:\\gui\\764972.png\""));
+        this.setHeaderText("Académico aceptado");
+        this.setContentText("Académico aceptado exitosamente");
+        this.getButtonTypes().add(new ButtonType("Aceptar"));
+
+    }
+
+}
