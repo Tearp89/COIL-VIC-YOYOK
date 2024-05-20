929b0e582157dc52b6a6e9d626bc59b8c90ba77e
Marla <121531470+Tearp89@users.noreply.github.com>
2024-05-19 18:35:10 -0600

    Update BDCoilVic195.sql

--- Base de datos/BDCoilVic195.sql
+++ Base de datos/BDCoilVic195.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `administrador`
 --
-
+USE `coil_vic`;
 DROP TABLE IF EXISTS `administrador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
